fa85e097681ecacf44b310bc804a870f4d9f3075
Jeongwon Park <sanguineman@naver.com>
2026-01-21 19:46:02 +0900

    add: pp for StructV

--- spectec/lib/interface/concrete/pp.ml
+++ spectec/lib/interface/concrete/pp.ml
@@ -44,7 +44,7 @@ let rec pp_value (hmap : hmap) fmt (value : value) : unit =
   | BoolV b -> F.fprintf fmt "%b" b
   | NumV n -> F.fprintf fmt "%a" pp_num n
   | TextV _ -> pp_text_v fmt value
-  | StructV _ -> failwith "@pp_value: StructV not implemented"
+  | StructV _ -> pp_struct_v hmap fmt value
   | CaseV _ -> pp_case_v hmap fmt value
   | TupleV values ->
       F.fprintf fmt "(%s)"
@@ -60,6 +60,21 @@ and pp_text_v fmt (value : value) : unit =
   match value.it with
   | TextV text -> F.fprintf fmt "%s" (String.escaped text)
   | _ -> failwith "@pp_text_v: expected TextV value"
+
+(* StructV *)
+
+and pp_struct_v (hmap : hmap) fmt (value : value) : unit =
+  match value.it with
+  | StructV fields ->
+    let field_strs =
+      fields
+      |> List.map (fun (atom, value) ->
+      F.asprintf "%s: %a"
+      (Atom.string_of_atom atom.it |> String.lowercase_ascii)
+      (pp_value hmap) value)
+    in
+    F.fprintf fmt "{\n%s,\n}" (String.concat ",\n" field_strs)
+  | _ -> failwith "@pp_struct_v: expected StructV value"
 
 (* CaseV *)
 
